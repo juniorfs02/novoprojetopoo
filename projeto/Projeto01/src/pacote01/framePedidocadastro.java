40d0dd3d478c46c2e9e800f7198407f302f5200d
Elivanf <junior_silwa@hotmail.com>
2021-07-08 14:14:15 -0300

    cadastro

--- projeto/Projeto01/src/pacote01/framePedidocadastro.java
+++ projeto/Projeto01/src/pacote01/framePedidocadastro.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -12,18 +13,21 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
 
 public class framePedidocadastro extends JFrame {
 	
 
 	private JPanel contentPane;
-	private JTextField textCodPedido;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField textCPnome;
+	private JTextField textCPcodped;
+	private JTextField textCPqtde;
+	private JTextField textCPfrete;
+	private JTextField textCPprecot;
+	private JTextField textCPcep;
+	private JTextField textCPlivro;
 
 	/**
 	 * Launch the application.
@@ -46,17 +50,17 @@ public class framePedidocadastro extends JFrame {
 	 */
 	public framePedidocadastro() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 413, 539);
+		setBounds(100, 100, 437, 539);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textCodPedido = new JTextField();
-		textCodPedido.setBounds(181, 120, 168, 26);
-		contentPane.add(textCodPedido);
-		textCodPedido.setColumns(10);
+		textCPnome = new JTextField();
+		textCPnome.setBounds(181, 120, 168, 26);
+		contentPane.add(textCPnome);
+		textCPnome.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Cadastro de Pedido");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -71,11 +75,10 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1.setBounds(44, 120, 98, 26);
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setColumns(10);
-		textField.setBounds(181, 83, 168, 26);
-		contentPane.add(textField);
+		textCPcodped = new JTextField();
+		textCPcodped.setColumns(10);
+		textCPcodped.setBounds(181, 83, 168, 26);
+		contentPane.add(textCPcodped);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("N\u00BA Pedido");
 		lblNewLabel_1_1.setForeground(Color.WHITE);
@@ -83,10 +86,10 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_1.setBounds(44, 83, 98, 26);
 		contentPane.add(lblNewLabel_1_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(181, 196, 168, 26);
-		contentPane.add(textField_1);
+		textCPqtde = new JTextField();
+		textCPqtde.setColumns(10);
+		textCPqtde.setBounds(181, 196, 168, 26);
+		contentPane.add(textCPqtde);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Quantidade");
 		lblNewLabel_1_2.setForeground(Color.WHITE);
@@ -94,10 +97,10 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_2.setBounds(44, 196, 127, 26);
 		contentPane.add(lblNewLabel_1_2);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(181, 270, 168, 26);
-		contentPane.add(textField_2);
+		textCPfrete = new JTextField();
+		textCPfrete.setColumns(10);
+		textCPfrete.setBounds(181, 270, 168, 26);
+		contentPane.add(textCPfrete);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Frete");
 		lblNewLabel_1_3.setForeground(Color.WHITE);
@@ -105,10 +108,10 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_3.setBounds(44, 270, 98, 26);
 		contentPane.add(lblNewLabel_1_3);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(181, 307, 168, 26);
-		contentPane.add(textField_3);
+		textCPprecot = new JTextField();
+		textCPprecot.setColumns(10);
+		textCPprecot.setBounds(181, 307, 168, 26);
+		contentPane.add(textCPprecot);
 		
 		JLabel lblNewLabel_1_4 = new JLabel("Pre\u00E7o Total");
 		lblNewLabel_1_4.setForeground(Color.WHITE);
@@ -116,19 +119,41 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_4.setBounds(44, 307, 98, 26);
 		contentPane.add(lblNewLabel_1_4);
 		
-		JButton btnpcConfirmar = new JButton("Confirmar ");
-		btnpcConfirmar.setForeground(Color.WHITE);
-		btnpcConfirmar.setFont(new Font("Bookman Old Style", Font.BOLD, 16));
-		btnpcConfirmar.setBackground(new Color(0, 153, 0));
-		btnpcConfirmar.setBounds(44, 406, 127, 29);
-		contentPane.add(btnpcConfirmar);
+		JButton btnCPconfirmar = new JButton("Confirmar ");
+		btnCPconfirmar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Pedidocadastroexe a = new Pedidocadastroexe();
+		        a.setCodpedido(Integer.parseInt(textCPcodped.getText()));
+		        a.setNomecliente(textCPnome.getText());
+		        a.setNomelivro(textCPlivro.getText());
+		        a.setQtde(Integer.parseInt(textCPqtde.getText()));
+		        a.setCep(Integer.parseInt(textCPcep.getText()));
+		        a.setValorfrete(Float.parseFloat(textCPfrete.getText()));
+		        a.setValortotal(Float.parseFloat(textCPprecot.getText()));
+		        try {
+		            a.Incluir();
+		        } catch (SQLException ex) {
+		            JOptionPane.showMessageDialog(null, ex);
+		        }
+			}
+		});
+		btnCPconfirmar.setForeground(Color.WHITE);		
+		btnCPconfirmar.setFont(new Font("Bookman Old Style", Font.BOLD, 16));
+		btnCPconfirmar.setBackground(new Color(0, 153, 0));
+		btnCPconfirmar.setBounds(44, 406, 127, 29);
+		contentPane.add(btnCPconfirmar);
 		
-		JButton btnpcVoltar = new JButton("Voltar");
-		btnpcVoltar.setForeground(Color.WHITE);
-		btnpcVoltar.setFont(new Font("Bookman Old Style", Font.BOLD, 16));
-		btnpcVoltar.setBackground(new Color(0, 153, 255));
-		btnpcVoltar.setBounds(228, 406, 121, 29);
-		contentPane.add(btnpcVoltar);
+		JButton btnCPvoltar = new JButton("Voltar");
+		btnCPvoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		btnCPvoltar.setForeground(Color.WHITE);
+		btnCPvoltar.setFont(new Font("Bookman Old Style", Font.BOLD, 16));
+		btnCPvoltar.setBackground(new Color(0, 153, 255));
+		btnCPvoltar.setBounds(228, 406, 121, 29);
+		contentPane.add(btnCPvoltar);
 		
 		JLabel lblNewLabel_1_2_1 = new JLabel("CEP");
 		lblNewLabel_1_2_1.setForeground(Color.WHITE);
@@ -136,10 +161,10 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_2_1.setBounds(44, 233, 127, 26);
 		contentPane.add(lblNewLabel_1_2_1);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(181, 233, 168, 26);
-		contentPane.add(textField_4);
+		textCPcep = new JTextField();
+		textCPcep.setColumns(10);
+		textCPcep.setBounds(181, 233, 168, 26);
+		contentPane.add(textCPcep);
 		
 		JLabel lblNewLabel_1_2_2 = new JLabel("Livro");
 		lblNewLabel_1_2_2.setForeground(Color.WHITE);
@@ -147,9 +172,14 @@ public class framePedidocadastro extends JFrame {
 		lblNewLabel_1_2_2.setBounds(44, 157, 127, 26);
 		contentPane.add(lblNewLabel_1_2_2);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(181, 157, 168, 26);
-		contentPane.add(textField_5);
+		textCPlivro = new JTextField();
+		textCPlivro.setColumns(10);
+		textCPlivro.setBounds(181, 157, 168, 26);
+		contentPane.add(textCPlivro);
+		
+		JButton btnCPbuscacep = new JButton("New button");
+		btnCPbuscacep.setBackground(new Color(0, 153, 255));
+		btnCPbuscacep.setBounds(358, 236, 27, 23);
+		contentPane.add(btnCPbuscacep);
 	}
 }
