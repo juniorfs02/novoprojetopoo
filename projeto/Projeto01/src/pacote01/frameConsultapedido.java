acc0415f1a1f019905c8b656fac9229d27a68cd0
Elivanf <junior_silwa@hotmail.com>
2021-07-08 19:46:06 -0300

    consulta

--- projeto/Projeto01/src/pacote01/frameConsultapedido.java
+++ projeto/Projeto01/src/pacote01/frameConsultapedido.java
@@ -8,18 +8,23 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class frameConsultapedido extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textCodPedido;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField textCOnome;
+	private JTextField textCOpedido;
+	private JTextField textCOdesc;
+	private JTextField textCOfrete;
+	private JTextField textCOprecot;
 
 	/**
 	 * Launch the application.
@@ -49,13 +54,36 @@ public class frameConsultapedido extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textCodPedido = new JTextField();
-		textCodPedido.setEditable(false);
-		textCodPedido.setBounds(181, 120, 168, 26);
-		contentPane.add(textCodPedido);
-		textCodPedido.setColumns(10);
+		textCOnome = new JTextField();
+		textCOnome.setEditable(false);
+		textCOnome.setBounds(181, 120, 168, 26);
+		contentPane.add(textCOnome);
+		textCOnome.setColumns(10);
 		
 		JButton btncpBuscarpedido = new JButton("Buscar");
+		btncpBuscarpedido.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Pedidocadastroexe a = new Pedidocadastroexe();
+				if(textCOpedido.getText().trim().isEmpty())
+	                a.setCodpedido(0);
+	            else
+	                a.setCodpedido(Integer.parseInt(textCOpedido.getText()));
+	        try {
+	            a.Pesquisar();
+	            textCOpedido.setEnabled(true);
+	            textCOnome.setText(a.getNomecliente());
+	            textCOdesc.setText("Nome: " + "\n" + a.getNomelivro() + "\n" +"Quantidade: " + a.getQtde());
+	            textCOfrete.setText(String.valueOf(a.getValorfrete()));
+	            textCOprecot.setText(String.valueOf(a.getValortotal()));
+	           
+	            
+	        } catch (SQLException ex) {
+	            JOptionPane.showMessageDialog(null, ex);
+	            
+	        }
+		}
+	});
+			
 		btncpBuscarpedido.setForeground(Color.WHITE);
 		btncpBuscarpedido.setFont(new Font("Bookman Old Style", Font.BOLD, 16));
 		btncpBuscarpedido.setBackground(new Color(0, 153, 0));
@@ -74,10 +102,10 @@ public class frameConsultapedido extends JFrame {
 		lblNewLabel_1.setBounds(44, 120, 98, 26);
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(181, 83, 168, 26);
-		contentPane.add(textField);
+		textCOpedido = new JTextField();
+		textCOpedido.setColumns(10);
+		textCOpedido.setBounds(181, 83, 168, 26);
+		contentPane.add(textCOpedido);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("N\u00BA Pedido");
 		lblNewLabel_1_1.setForeground(Color.WHITE);
@@ -85,11 +113,11 @@ public class frameConsultapedido extends JFrame {
 		lblNewLabel_1_1.setBounds(44, 83, 98, 26);
 		contentPane.add(lblNewLabel_1_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
-		textField_1.setBounds(181, 157, 168, 102);
-		contentPane.add(textField_1);
+		textCOdesc = new JTextField();
+		textCOdesc.setEditable(false);
+		textCOdesc.setColumns(10);
+		textCOdesc.setBounds(181, 157, 168, 102);
+		contentPane.add(textCOdesc);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Descri\u00E7\u00E3o Pedido");
 		lblNewLabel_1_2.setForeground(Color.WHITE);
@@ -97,23 +125,23 @@ public class frameConsultapedido extends JFrame {
 		lblNewLabel_1_2.setBounds(44, 157, 127, 26);
 		contentPane.add(lblNewLabel_1_2);
 		
-		textField_2 = new JTextField();
-		textField_2.setEditable(false);
-		textField_2.setColumns(10);
-		textField_2.setBounds(181, 270, 168, 26);
-		contentPane.add(textField_2);
+		textCOfrete = new JTextField();
+		textCOfrete.setEditable(false);
+		textCOfrete.setColumns(10);
+		textCOfrete.setBounds(181, 270, 168, 26);
+		contentPane.add(textCOfrete);
 		
 		JLabel lblNewLabel_1_3 = new JLabel("Frete");
 		lblNewLabel_1_3.setForeground(Color.WHITE);
 		lblNewLabel_1_3.setFont(new Font("Bookman Old Style", Font.BOLD, 14));
-		lblNewLabel_1_3.setBounds(44, 270, 98, 26);
+		lblNewLabel_1_3.setBounds(44, 269, 98, 26);
 		contentPane.add(lblNewLabel_1_3);
 		
-		textField_3 = new JTextField();
-		textField_3.setEditable(false);
-		textField_3.setColumns(10);
-		textField_3.setBounds(181, 307, 168, 26);
-		contentPane.add(textField_3);
+		textCOprecot = new JTextField();
+		textCOprecot.setEditable(false);
+		textCOprecot.setColumns(10);
+		textCOprecot.setBounds(181, 307, 168, 26);
+		contentPane.add(textCOprecot);
 		
 		JLabel lblNewLabel_1_4 = new JLabel("Pre\u00E7o Total");
 		lblNewLabel_1_4.setForeground(Color.WHITE);
